ca7dd4a290bd9b4729569fd92dd466f3f439d8c4
Younghyun Lee <76776828+yh-lee371@users.noreply.github.com>
2021-03-09 11:01:07 +0900

    쿠키 확인

---
+++ toggleBtn4.js
@@ -0,0 +1,34 @@
+$(function(){
+  // 요소의 기본 성격 제거
+  $('a[href="#"]').on('click',function(e){
+    e.preventDefault();
+  });
+
+  // 배너
+  /*
+  $('.close').on('click',function(){
+    $('.banner').toggleClass('on');
+  });
+  */
+
+  // 선언함수: 이름이 있는 함수. 이름으로 호출할 수 있으므로 작성 위치가 자유로움
+  function closeBanner(){ // closeBanner <- 사용자가 정의한 이름
+
+    let $limit = $('#limit');
+    if($limit.is(':checked')){ // 체크박스에 체크되면, 
+      $.cookie('popup','none',{expires:3, path: '/'})// 지정된 기간 동안 열지 않음.
+
+    }
+
+    $('.banner').toggleClass('on');
+  };
+
+  $('.close_btn').on('click', closeBanner);
+
+
+  //슬라이드
+  $('.tab li').on('click',function(){
+    let idx = $(this).index();
+    $('.slide').animate({marginLeft:-640 * idx},600);
+  });
+});
